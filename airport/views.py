c5c52cc1cfdc68857d2fd742ae830b9c45f169e7
S-SMTN <cven28@gmail.com>
2024-07-04 16:58:41 +0300

    installed DRF, added AirportSerializer, AirportViewSet and registered router "airports"

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins, status
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from airport.models import Airport
+from airport.serializers import AirportSerializer
+
+
+class AirportViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    GenericViewSet
+):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+    #permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
